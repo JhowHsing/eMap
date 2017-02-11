9bdc8c7b30c7e61a64b215065c3337a63fb5cef5
JhowHsing <J910X214@ku.edu>
2017-02-11 19:08:31 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -45,7 +45,7 @@ $.get('https://s3-us-west-2.amazonaws.com/s.cdpn.io/95368/USA_geo.json', functio
             min: 0,
             max: 500,
             inRange: {
-                color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
+                color: ['#424242', '#616161', '#757575', '#9E9E9E', '#E0E0E0', '#EEEEEE', '#DCEDC8', '#C5E1A5', '#AED581', '#9CCC65','#8BC34A']
             },
             text:['Opening','Wait to start'],           // 文本，默认为数值文本
             calculable: true
